4647e14c6cc5a97de31fa7d37fcbeb497268ca6b
Joe Watt <joewatt95@u.nus.edu>
2024-02-13 14:06:17 +0800

    reoder clauses, add a note

--- src/homecoming_server_status/lib/database/datalog_ruleset/player_stats.clj
+++ src/homecoming_server_status/lib/database/datalog_ruleset/player_stats.clj
@@ -7,23 +7,26 @@
  "∀ ?shard ?num_playing ?date_time,
     is_the_most_populated_shard_with_players_as_of(?shard, ?num_playing, ?date_time) ←
       is_shard_as_of(?shard, ?date_time) ∧
+      playing(?shard, ?num_playing) ∧
       is_the_largest_such_that(
         ?num_playing, ?num_playing', (
           ∃ ?shard',
             is_shard_as_of(?shard', ?date_time) ∧
             playing(?shard', ?num_playing')
         )
-      ) ∧
-      playing(?shard, ?num_playing)"
+      )"
  '[(is-the-most-populated-shard-with-players-as-of ?ruleset ?shard ?num-playing ?date-time)
    (is-shard-as-of ?shard ?date-time)
-   ;; Note: Need to explicitly pass ruleset to inner queries in this way.
-   ;; They don't inherit it implicitly like with data in the database.
-   [(q '[:find (max ?num-playing)
-         :in $ % ?date-time
+   [?shard :playing ?num-playing]
+   ;; Note:
+   ;; - Need to explicitly pass ruleset to inner queries in this way.
+   ;;   They don't inherit it implicitly like with data in the database.
+   ;; - Params that are to be unified with the outer query have to be passed
+   ;;   in explicitly or they'll be implicitly existentially quantified over.
+   [(q '[:find (max ?num-playing) ?date-time
+         :in $ %
          :where
          (is-shard-as-of ?shard' ?date-time)
          [?shard' :playing ?num-playing]]
-       $ ?ruleset ?date-time)
-    [[?num-playing]]]
-   [?shard :playing ?num-playing]])
+       $ ?ruleset)
+    [[?num-playing ?date-time]]]])
